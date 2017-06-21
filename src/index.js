c3a6136028ca98efa4ebca27bfc7f4a899df3f3f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-06-21 12:22:53 +0900

    Split slidekit.js into separate js files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var sk;
+const slides = document.querySelector('#slides');
+slides.addEventListener('load', function(e) {
+    sk = slidekit(this.contentDocument);
+
+    // Move to the first slide
+    sk.gotoSlide(0);
+
+    // Key bindings
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode === 0 || e.keyCode === 39) {
+            // next
+            sk.nextSlide();
+        }
+        else if (e.keyCode === 8 || e.keyCode === 37) {
+            // previous
+            sk.prevSlide();
+        }
+    });
+});
--- src/slidekit.js
+++ src/slidekit.js
@@ -28,24 +28,3 @@ const slidekit = function(svgDoc) {
 
     return module;
 };
-
-var sk;
-const slides = document.querySelector('#slides');
-slides.addEventListener('load', function(e) {
-    sk = slidekit(this.contentDocument);
-
-    // Move to the first slide
-    sk.gotoSlide(0);
-
-    // Key bindings
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode === 0 || e.keyCode === 39) {
-            // next
-            sk.nextSlide();
-        }
-        else if (e.keyCode === 8 || e.keyCode === 37) {
-            // previous
-            sk.prevSlide();
-        }
-    });
-});
